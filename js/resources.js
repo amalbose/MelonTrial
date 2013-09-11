5c8978308adb7c14dd5465ab7ab5170200ff4272
Amal Bose <axatrikx@gmail.com>
2013-09-11 21:00:52 +0530

    Updated resources, game and play js files

--- js/resources.js
+++ js/resources.js
@@ -1,28 +1,8 @@
 game.resources = [
 
-	/* Graphics. 
-	 * @example
-	 * {name: "example", type:"image", src: "data/img/example.png"},
-	 */
-	
-	/* Atlases 
-	 * @example
-	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
-	 */
-		
-	/* Maps. 
-	 * @example
-	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
-	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
- 	 */
+	{name: "area01_level_tiles",  type:"image", src: "data/img/map/area01_level_tiles.png"},     
+    {name: "area01", type: "tmx", src: "data/map/area01.tmx"},
 
-	/* Background music. 
-	 * @example
-	 * {name: "example_bgm", type: "audio", src: "data/bgm/", channel : 1},
-	 */	
-	
-	/* Sound effects. 
-	 * @example
-	 * {name: "example_sfx", type: "audio", src: "data/sfx/", channel : 2}
-	 */
+    {name: "area01_bkg0",         type:"image", src: "data/img/area01_bkg0.png"},
+    {name: "area01_bkg1",         type:"image", src: "data/img/area01_bkg1.png"},
 ];
