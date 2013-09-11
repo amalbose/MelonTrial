a06ad1ce27a1a5e7691f3e82d368c16a18aa3a9d
Amal Bose <axatrikx@gmail.com>
2013-09-11 21:55:20 +0530

    Reformatted

--- js/game.js
+++ js/game.js
@@ -11,8 +11,8 @@ var game = {
         // add "#debug" to the URL to enable the debug Panel
         if (document.location.hash === "#debug") {
             window.onReady(function () {
-                me.plugin.register.defer(debugPanel, "debug");
-            });
+               me.plugin.register.defer(debugPanel, "debug");
+           });
         }
  
         // Initialize the audio.
--- js/resources.js
+++ js/resources.js
@@ -1,8 +1,7 @@
 game.resources = [
-
 	{name: "area01_level_tiles",  type:"image", src: "data/img/map/area01_level_tiles.png"},     
-    {name: "area01", type: "tmx", src: "data/map/area01.tmx"},
-
-    {name: "area01_bkg0",         type:"image", src: "data/img/area01_bkg0.png"},
-    {name: "area01_bkg1",         type:"image", src: "data/img/area01_bkg1.png"},
+	{name: "area01", type: "tmx", src: "data/map/area01.tmx"},
+	{name: "area01_bkg0",         type:"image", src: "data/img/area01_bkg0.png"},
+	{name: "area01_bkg1",         type:"image", src: "data/img/area01_bkg1.png"},
+	{name: "metatiles32x32",         type:"image", src: "data/img/map/metatiles32x32.png"},
 ];
