5d79c38ccbc5b9fd29fe4b1ed59c3f2ae68d7c75
Amal Bose <axatrikx@gmail.com>
2013-09-12 19:15:50 +0530

    Added player and movement

--- js/resources.js
+++ js/resources.js
@@ -1,7 +1,10 @@
 game.resources = [
-	{name: "area01_level_tiles",  type:"image", src: "data/img/map/area01_level_tiles.png"},     
-	{name: "area01", type: "tmx", src: "data/map/area01.tmx"},
+	{name: "area01_level_tiles",  type:"image", src: "data/img/map/area01_level_tiles.png"},
+
+	{name: "area01", type: "tmx", src: "data/map/area02.tmx"},
+	
 	{name: "area01_bkg0",         type:"image", src: "data/img/area01_bkg0.png"},
 	{name: "area01_bkg1",         type:"image", src: "data/img/area01_bkg1.png"},
 	{name: "metatiles32x32",         type:"image", src: "data/img/map/metatiles32x32.png"},
+	{name: "gripe_run_right", type:"image", src: "data/img/sprite/gripe_run_right.png"},
 ];
